2f7e583b8aa5215d51d2281b9d7361686848cb46
reehfpal53 <reehfpal53@gmail.com>
2026-06-05 17:26:02 +0900

    feat: 상품 페이징 및 키워드 검색 기능 구현

--- src/main/java/kr/ac/hansung/service/ProductService.java
+++ src/main/java/kr/ac/hansung/service/ProductService.java
@@ -4,10 +4,11 @@ import kr.ac.hansung.dto.ProductDto;
 import kr.ac.hansung.entity.Product;
 import kr.ac.hansung.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -17,8 +18,13 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     @Transactional(readOnly = true)
-    public List<Product> findAll() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Product> searchProducts(String keyword, Pageable pageable) {
+        return productRepository.findByNameContaining(keyword, pageable);
     }
 
     @Transactional(readOnly = true)
